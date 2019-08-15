2aa34e733782faf791ffc5cc45fd6e6439214b5e
xiaodisgod <root@localhost.localdomain>
2019-08-15 09:36:05 -0400

    opencv_second_yolo

--- opencv_yolov3.c
+++ opencv_yolov3.c
@@ -35,5 +35,39 @@ int main(int argc,char** argv)
     while(getline(ifs,line)) classes.push_back(line);
     string modelConfiguration="yolov3.cfg";
     string modelWeights="yolov3.weights";
-    Net 
+    Net net=readNetFromDarknet(modelConfiguration,modelWeights);
+    net.setPreferableBackend(modelConfiguration,modelWeights);
+    net.setPreferableTarget(DNN_TARGET_CPU);
+    string str,outputFile;
+    VideoCapture cap;
+    VideoWriter video;
+    Mat Frame,blob;
+    try{
+        outputFile="yolo_out_cpp.avi";
+        if(parser.has("image"))
+        {
+            str=parser.get<string>("image");
+            ifstream ifile(str);
+            if(!ifile) throw("error");
+            cap.open(str);
+            str.replace(str.end()-4,str.end(),"_yolo_out_cpp.jpg");
+            outputFile=str;
+        }
+        else if(parser.has("video"))
+        {
+            str=parser.get<string>("video");
+            ifstream ifile(str);
+            cap.open(str);
+            str.replace(str.end()-4,str.end(),"_yolo_out_cpp.avi");
+            outputFile=str;
+        }
+        else cap.open(parser.get<int>("device"));
+    }
+    catch(...){
+        cout<<"could not open the input image/video stream"<<endl;
+        return 0;
+    }
+    if(!parser.has("image")){
+        video.open(outputFile,VideoWriter::fourcc('M','J','P',"G"),28,Size(cap.get(CAP_PROP_FRAME_WIDTH,cap.get(CAP_PROP_FRAME_HEIGHT)));
+    }
 }
